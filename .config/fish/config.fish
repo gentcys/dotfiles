1421812b966d5892fc1d1a37e97db5722064c817
gentcys <iamchencong@gmail.com>
2020-03-13 15:19:46 +0800

    add pyenv and pyenv-virtualenv concerned configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,9 +9,6 @@ alias dotfiles="/usr/local/bin/git --git-dir=$HOME/Codes/personal/dotfiles/ --wo
 # Homebrew's sbin
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 
-# rbenv
-status --is-interactive; and source (rbenv init -|psub)
-
 # golang
 set -gx GOPATH $HOME/Codes/gocode
 set -gx PATH $PATH $GOPATH/bin
@@ -27,3 +24,10 @@ set -gx ANDROID_HOME /usr/local/share/android-sdk
 set -gx PATH $ANDROID_HOME/tools $PATH
 set -gx PATH $ANDROID_HOME/build-tools $PATH
 set -gx PATH $ANDROID_HOME/platform-tools $PATH
+
+# rbenv
+status --is-interactive; and source (rbenv init -|psub)
+
+# pyenv
+status --is-interactive; and pyenv init - | source
+status --is-interactive; and pyenv virtualenv-init - | source
